f1b9429b67c2374ed27554aabc870f2685bc6536
Taner <tanerozer16@gmail.com>
2021-09-15 00:44:53 +0300

    ana component ayarlandı

--- src/component/contacts/index.js
+++ src/component/contacts/index.js
@@ -1,30 +1,40 @@
 import { useState, useEffect } from "react";
 import "./style.css";
-import List from "./List";
-import Form from "./Form";
+import List from "./List/List";
+import Form from "./Form/Form";
 
 function Contacts() {
   const [contacts, setContacts] = useState([
     {
       fullName: "Taner",
-      unvan: "Bs",
       phoneNumber: "5548",
     },
     {
-      fullName: "bamkimum",
-      unvan: "Bs",
+      fullName: "Fatih",
       phoneNumber: "555",
     },
     {
       fullName: "Hasan",
-      unvan: "Gs",
       phoneNumber: "4566",
+    },
+    {
+      fullName: "Hamid",
+      phoneNumber: "3377",
+    },
+    {
+      fullName: "Ela",
+      phoneNumber: "1298",
+    },
+    {
+      fullName: "Arsihan",
+      phoneNumber: "8914",
     },
   ]);
 
   useEffect(() => {
-    console.log(contacts);
+    // console.log(contacts);
   }, [contacts]);
+
   return (
     <div id="Con">
       <h1>Contacts</h1>
